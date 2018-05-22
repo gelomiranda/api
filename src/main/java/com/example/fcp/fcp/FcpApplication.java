55e17466d2c5fe9f7c551ae3c11b9b54b4e312fb
angelogarcia <angelogarcia@deltek.com>
2018-05-22 20:00:32 +0800

    first commit

---
+++ src/main/java/com/example/fcp/fcp/FcpApplication.java
@@ -0,0 +1,12 @@
+package com.example.fcp.fcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcpApplication.class, args);
+	}
+}
